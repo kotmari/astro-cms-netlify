f20e2a953a911dac5dd5d5ce6656dd9c6b61d539
kotmari <mariannakot79@gmail.com>
2024-12-05 13:24:27 +0200

    test netlify

---
+++ astro.config.mjs
@@ -0,0 +1,20 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import tailwind from '@astrojs/tailwind';
+
+
+
+import icon from 'astro-icon';
+
+
+
+import netlify from '@astrojs/netlify';
+
+
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind(), icon()],
+  adapter: netlify()
+});
